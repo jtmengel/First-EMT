f948766e7fdf3d187ac49a42da302cb51c2a05df
JT Mengel <JTMengel@gmail.com>
2011-08-04 12:13:25 -0400

    Old gods, so on and so forth, and now I deleted some unnecessary code.

--- dynamic_example.js
+++ dynamic_example.js
@@ -126,13 +126,6 @@ function printQuestions()
 			$( question_group_list[h][0][2] ).append($question_div);
 			var $anchor = $('<a href="media/video/'+ (question_group_list[h][i][0].getElementsByTagName(patient_lang))[0].getElementsByTagName('video')[0].childNodes[0].nodeValue.replace(/^\s+|\s+$/g, '') + '" id="anchor" class ="' + patient_lang + '" caption="' + (question_group_list[h][i][0].getElementsByTagName(patient_lang))[0].childNodes[0].nodeValue + '" >');
 
-//	        if( question_group_list[h][i][1][0] === undefined ){
-//		        question_group_list[h][i][1][0]  =  false;
-//        		question_group_list[h][i][1][1]  =  $('<div class="button neutral">!</div>');
-//    		}
-//			$question_div.append(question_group_list[h][i][1][1] );
-//			assignButtons( question_group_list[h][i][1] );
-
 			$question_div.append($anchor);
 			var $question = $('<div class="question">');
 			$anchor.append($question);
